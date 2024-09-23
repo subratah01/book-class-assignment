8e272aabe2987656414ec69bb74acf33bbe3bcf1
subratah01 <subratah01@gmail.com>
2024-09-23 19:06:08 +0530

    Code updated

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,18 @@
+import 'package:book_class_assignment/book.dart';
 void main(){
-  print("Hello World!");
+  Book objBook1= Book("Eloquent JavaScript","Marijn Haverbeke",2018);
+  objBook1.read(4);
+  objBook1.getBookDetails();
+
+  Book objBook2= Book("Practical Modern JavaScript","Nicolás Bevacqua",2017);
+  objBook2.read(13);
+  objBook2.getBookDetails();
+
+  Book objBook3= Book("Let Us C","Yasbant Kanitkar",2013);
+  objBook3.read(10);
+  objBook3.getBookDetails();
+
+  print("===========================================");
+  print("Total Books Created: ${Book.totalBooks}");
+  print("===========================================");
 }
---
+++ lib/book.dart
@@ -0,0 +1,41 @@
+class Book{
+  String _title="";
+  String _author="";
+  int _publicationYear=0;
+  int _pagesRead=0;
+  static int totalBooks=0;
+
+  Book(String title,String author, int publicationYear){
+    _title=title;
+    _author=author;
+    _publicationYear=publicationYear;
+    totalBooks++;
+  }
+  getTitle(){ return _title; }
+  getAuthor(){ return _author; }
+  getPublicationYear(){ return _publicationYear; }
+  getBookAge(){
+    int currentYear = DateTime.now().year;
+    int bookAge=currentYear-_publicationYear;
+    return bookAge;
+  }
+  getPagesRead(){
+    return _pagesRead;
+  }
+  read(int pages){
+    _pagesRead +=pages;
+  }
+
+  getBookDetails(){
+    print ("==========================================");
+    print ("Book-$totalBooks details are as follows:");
+    print ("==========================================");
+    print("Title: "+ getTitle());
+    print("Author: "+ getTitle());
+    print("Publication Year: "+ getPublicationYear().toString());
+    print("Book Age: "+ getBookAge().toString());
+    print("Pages Read: "+ getPagesRead().toString());
+    print("");
+  }
+
+}
