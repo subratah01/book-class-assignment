f3905a69dfa955fe2d69480e61440952cf15e1fd
subratah01 <subratah01@gmail.com>
2024-09-23 17:53:39 +0530

    dart test file deleted

--- test/book_class_assignment_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:book_class_assignment/book_class_assignment.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
